9cdc79e7aee3adfbb22b69c242527167ca15731f
twl8n <tom@laudeman.com>
2017-03-16 21:55:04 -0400

    Clean up.

--- src/machine/core.clj
+++ src/machine/core.clj
@@ -4,6 +4,7 @@
   (:gen-class))
 
 ;; https://github.com/clojure/tools.namespace
+;; (tns/refresh)
 
 (defn clean-line [str]
   (-> str
@@ -13,19 +14,7 @@
       (str/replace #"^[\s\|]*$" "")
       ))
 
-;; (let [good-str (str/replace str #"^\|\s*" "")]
-;;      good-str))
-
-;; (filter (fn foo [xx] (> 1 (count xx))) )
-
-;; (defn save-this-line []
-;;   (reduce (fn [keylist vallist] 
-;;             (conj keylist (zipmap (vec (keys (first keylist))) vallist))
-;;             ) 
-;;           (rest all-lines)))
-;; [(zipmap (first all-lines) (take (count (first all-lines)) (range)))]
-
-(def colseq [:edge :test :func :next])
+(def col-name [:edge :test :func :next])
 
 ;; http://stackoverflow.com/questions/6135764/when-to-use-zipmap-and-when-map-vector
 ;; Use (zipmap ...) when you want to directly construct a hashmap from separate sequences of keys and values. The output is a hashmap:
@@ -37,9 +26,7 @@
                       this-line))
                         (str/split all-lines #"\n")))
         good-lines (filter (fn foo [xx] (> (count xx) 1)) lseq)]
-    (map #(zipmap colseq %) good-lines)))
-
-
+    (map #(zipmap col-name %) good-lines)))
 
 (defn -main
   "Parse the states.dat file."
