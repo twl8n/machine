f53dc2681d054f12e3cd7b67bf954806476bb8ba
twl8n <tom@laudeman.com>
2017-03-30 22:22:52 -0400

    Start creating function traverse, recursive.

--- src/machine/core.clj
+++ src/machine/core.clj
@@ -19,6 +19,35 @@
 ;; http://stackoverflow.com/questions/6135764/when-to-use-zipmap-and-when-map-vector
 ;; Use (zipmap ...) when you want to directly construct a hashmap from separate sequences of keys and values. The output is a hashmap:
 
+;; table is a global, read only.
+(def table [{:edge "login", :test "if-logged-in", :func "", :next "pages"}])
+
+;; function names are symbols
+(defn ex1 []
+  (defn baz [] (prn "fn baz"))
+  (prn "first:")
+  ((:foo {:foo (eval (read-string "baz"))}))
+  (prn "second:")
+  ((:foo {:foo (eval 'baz)})))
+
+  ;; The only :else for both cond's would be logging. The :else of the inner cond should never be reached.
+  ;; The :else of the outer cond happens all the time as we iterate through the current state.
+(defn traverse
+  "Must have a starting state. jump-stack initially is empty. Return a map with keys wait-next, msg."
+  [curr-state jump-stack]
+  (doseq [smap table] ;; state map, state table
+    (cond (and (= (smap :edge) curr-state)
+               (dispatch (smap :test)))
+          (cond (nil? (smap :func))
+                (traverse (smap :next) jump-stack)
+                (jump? (smap :func))
+                (traverse (jump-to (smap :func)) (conj jump-stack (smap :next)))
+                (return? (smap :func))
+                (traverse (first jump-stack) (rest jump-stack))
+                (wait? (smap :func))
+                (smap :next)))))
+
+
 (defn read-state-file []
   (let [all-lines (slurp "states.dat")
         lseq (rest (map (fn bar [one-line]
